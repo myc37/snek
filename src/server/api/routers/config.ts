e03d245ecd204d248533d95bbfa4588901ada644
zechajw <zecharyajw@gmail.com>
2023-02-25 15:42:02 +0800

    Config Routers

--- src/server/api/routers/config.ts
+++ src/server/api/routers/config.ts
@@ -6,5 +6,26 @@ import { Country } from "@prisma/client";
 export const configsRouter = createTRPCRouter({
   getConfigByCountry: publicProcedure
     .input(z.object({ country: z.nativeEnum(Country) }))
-    .query(async ({ input: { country } }) => {}),
+    .query(async ({ input: { country } }) => {
+      return await prisma.countryConfig.findUnique({ where: { country } });
+    }),
+  putConfigByCountry: publicProcedure
+    .input(
+      z.object({
+        country: z.nativeEnum(Country),
+        isInfractionVisible: z.boolean(),
+        infractionPayStructure: z.any(),
+      })
+    )
+    .mutation(
+      async ({
+        input: { country, isInfractionVisible, infractionPayStructure },
+      }) => {
+        return await prisma.countryConfig.upsert({
+          where: { country },
+          update: { isInfractionVisible, infractionPayStructure },
+          create: { isInfractionVisible, infractionPayStructure, country },
+        });
+      }
+    ),
 });
