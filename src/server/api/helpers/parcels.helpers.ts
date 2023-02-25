2efad9964ce25fb3165ff4e451f13cfce96d0da7
myc37 <nvjn37@gmail.com>
2023-02-25 22:18:25 +0800

    fix: add month range constraints for parcel api

--- src/server/api/helpers/parcels.helpers.ts
+++ src/server/api/helpers/parcels.helpers.ts
@@ -1,36 +1,39 @@
 import { ParcelStatus } from "@prisma/client";
 import { prisma } from "~/server/db";
 
+const getMonthRange = () => {
+  const start = new Date();
+  const end = new Date();
+
+  start.setDate(1);
+  start.setHours(0, 0, 0, 0);
+  end.setMonth(start.getMonth() + 1);
+  end.setDate(1);
+  end.setHours(0, 0, 0, 0);
+
+  return { start, end };
+};
+
 export const getCompletedParcelsByDriverId = async (driverId: string) => {
+  const { start, end } = getMonthRange();
+
   return await prisma.parcel.findMany({
     where: {
       driverId,
-      status: "DELIVERED",
+      status: ParcelStatus.DELIVERED,
+      deliveryDate: { gte: start, lt: end },
     },
   });
 };
 
 export const getNonCompletedParcelsByDriverId = async (driverId: string) => {
+  const { start, end } = getMonthRange();
+
   return await prisma.parcel.findMany({
     where: {
       driverId,
-      NOT: { status: "DELIVERED" },
+      NOT: { status: ParcelStatus.DELIVERED },
+      assignedDate: { gte: start, lt: end },
     },
   });
-};
-
-export const updateParcelStatusByTrackingNumber = async (
-  trackingNumber: string,
-  status: ParcelStatus
-) => {
-  await prisma.parcel
-    .update({
-      where: { trackingNumber },
-      data: { status },
-    })
-    .then(async (updatedParcel) => {
-      const driver = await prisma.driver.findUnique({
-        where: { id: updatedParcel.driverId },
-      });
-    });
 };
