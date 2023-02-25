dcb2d237a0cc38ad78a0b19a1efbe4cc3db72034
myc37 <nvjn37@gmail.com>
2023-02-25 21:59:45 +0800

    feat: add quest bonus route

--- src/server/api/routers/quests.ts
+++ src/server/api/routers/quests.ts
@@ -1,12 +1,20 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { getAllQuestInstancesForDriver } from "../helpers/quests.helpers";
+import {
+  getAllQuestInstancesForDriver,
+  getQuestBonusForDriver,
+} from "../helpers/quests.helpers";
 
 export const questsRouter = createTRPCRouter({
   getAllQuestsByDriverId: publicProcedure
     .input(z.object({ driverId: z.string() }))
     .query(async ({ input: { driverId } }) => {
       return await getAllQuestInstancesForDriver(driverId);
+    }),
+  getQuestBonusByDriverId: publicProcedure
+    .input(z.object({ driverId: z.string() }))
+    .query(async ({ input: { driverId } }) => {
+      return await getQuestBonusForDriver(driverId);
     }),
 });
